ea207e1b6be448b52f88a39fb5c1f1624c47f9bc
zeyaoli <zeyao.li.315@gmail.com>
2020-03-18 23:11:22 -0400

    Shift from nedb to mongodb

--- public/main.js
+++ public/main.js
@@ -4,58 +4,63 @@ window.addEventListener('DOMContentLoaded', () => {
 	fetch('/api')
 		.then((res) => res.json())
 		.then((data) => {
-			lists = data;
-			let list = getList(lists);
-			console.log(list);
+			console.log(data);
+			// let list = getList(lists);
+			// console.log(list);
 			// return list.map((laura) => {
 			// 	// here are all the variables i need !
 			// 	// console.log(`${laura.name}  ${laura.year}`);
 			// });
-			function Header() {
-				return `<div class="header">
-							<li>Home</li>
-							<li>Documentation</li>
-							<li>About</li>
-						</div>`;
-			}
 
-			function Card(cardData) {
-				return `<div class="card">
-							<img src="${cardData.image}" width="300px"/>
-							<h2>${cardData.name}</h2>
-							<p>${cardData.year}</p>
-							<h4>${cardData.character}</h4>
-						</div>`;
-			}
-
-			function Cards(posts) {
-				return `<div class="cards">
-					${posts.map(Card).join('')}
-				</div>`;
-			}
-
-			function Footer() {
-				return `<footer> Developed by <a href="https://zeyaoli.com">Zeyao Li</a> 2020 </footer>`;
-			}
-			function HomePage() {
-				return `<div class="page">
-						${Header()}
-						<h1 id="title">Laura Dern API</h1>
-						<p id="subtitle">🏳️‍🌈Let's just say... gay rights!!🏳️‍🌈</p>
-						${Cards(list)}
-						${Footer()}
-						</div>`;
-			}
-
-			document.getElementById('root').innerHTML = HomePage();
+			document.getElementById('root').innerHTML = HomePage(data);
 		})
 		.catch((err) => {
 			console.log(err);
 		});
 });
 
-const getList = (lists) => {
-	return lists.map((list) => {
-		return list.list;
-	});
-};
+// const getList = (lists) => {
+// 	return lists.map((list) => {
+// 		return list.list;
+// 	});
+// };
+
+function Header() {
+	return `<div class="header">
+				<li>Home</li>
+				<li>Documentation</li>
+				<li><a href="/about">About</a></li>
+			</div>`;
+}
+
+function Card(cardData) {
+	return `<div class="card">
+				<img src="${cardData.image}" width="300px"/>
+				<h2>${cardData.name}</h2>
+				<p>${cardData.year}</p>
+				<h4>${cardData.character}</h4>
+			</div>`;
+}
+
+function Cards(posts) {
+	return `<div class="cards">
+		${posts.map(Card).join('')}
+	</div>`;
+}
+
+function Footer() {
+	return `<footer> Developed by <a href="https://zeyaoli.com">Zeyao Li</a> 2020 </footer>`;
+}
+function HomePage(cardData) {
+	return `<div class="page">
+			${Header()}
+			<h1 id="title">Laura Dern API</h1>
+			<p id="subtitle">🏳️‍🌈Let's just say... gay rights!!🏳️‍🌈</p>
+			${Cards(cardData)}
+			${Footer()}
+			</div>`;
+}
+
+// function AboutPage(){
+// 	return
+// }
--- server.js
+++ server.js
@@ -1,11 +1,32 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
-const Datastore = require('nedb');
+const mongoose = require('mongoose');
+// const Datastore = require('nedb');
 const viewsURL = path.resolve(`${__dirname}/public`);
 const app = express();
 
-const db = new Datastore({ filename: `lauradern.db`, autoload: true });
+require('dotenv').config();
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose.connect(MONGO_URL, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+	console.log('connected to database');
+});
+
+const ListSchema = mongoose.Schema({
+	type      : String,
+	name      : String,
+	year      : Number,
+	character : String,
+	image     : String
+});
+
+const List = mongoose.model('List', ListSchema);
+
+// const db = new Datastore({ filename: `lauradern.db`, autoload: true });
 // console.log(db);
 
 app.use(express.static(viewsURL));
@@ -15,31 +36,57 @@ app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'views/index.html'));
 });
 
+app.get('/about', (req, res) => {
+	res.sendFile(path.join(__dirname, 'views/about.html'));
+});
+
+// const getData = (cb) => {
+// 	db.find({}).sort({ 'list.year': -1 }).exec((err, lists) => {
+// 		cb(err, lists);
+// 	});
+// };
+
 const getData = (cb) => {
-	db.find({}, (err, lists) => {
-		cb(err, lists);
+	List.find({}, (err, docs) => {
+		cb(err, docs);
 	});
 };
 
 const addData = (data, cb) => {
-	db.insert({ list: data }, (err, newData) => {
+	// db.insert({ list: data }, (err, newData) => {
+	// 	cb(err, newData);
+	// });
+	List.create(data, (err, newData) => {
 		cb(err, newData);
 	});
 };
 
 app.get('/api', (req, res) => {
-	getData((err, lists) => {
-		res.json(lists);
+	// const year = req.query.year;
+	// if(req.query.type) else
+	// getData((err, lists) => {
+	// 	res.json(lists);
+	// });
+	getData((err, docs) => {
+		res.json(docs);
 	});
 });
 
+app.get('/api/random', (req, res) => {
+	getData((err, docs) => {
+		res.json(docs[Math.floor(Math.random() * docs.length)]);
+	});
+});
+
+// app.get('/api/type');
+
 app.post('/api', (req, res) => {
-	const newList = req.body;
-	addData(newList, (err, newData) => {
+	const newData = req.body;
+	addData(newData, (err, newData) => {
 		res.json(newData);
 	});
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
 	console.log(`Server is listening on port 3000`);
 });
